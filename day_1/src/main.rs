534fef411f3de27e6bfd5580742ba427555416a1
jappelgren <justinappelgren@gmail.com>
2022-12-01 12:45:21 -0600

    line break added

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -22,6 +22,7 @@ fn second_star(sorted_cals: Vec<i32>) -> i32 {
 fn main() {
     let sorted_cals: Vec<i32> = first_star();
     println!("The largest amount of calories carried by one elf is {:#?}.", sorted_cals[sorted_cals.len() - 1]);
+    
     let summed_top_three: i32 = second_star(sorted_cals);
     println!("The sum of the top three elves calories is {:#?}.", summed_top_three);
 }
