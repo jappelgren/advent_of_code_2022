e66ff3d0c53ca8244add99163345544f310be290
jappelgren <justinappelgren@gmail.com>
2022-12-01 12:57:38 -0600

    formatted with rust-fmt

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,13 +1,20 @@
 use std::fs;
 
 fn first_star() -> Vec<i32> {
-    let calories: String = fs::read_to_string(".\\src\\calories.txt").unwrap().replace("\r\n\r", ",").replace('\n', "");
+    let calories: String = fs::read_to_string(".\\src\\calories.txt")
+        .unwrap()
+        .replace("\r\n\r", ",")
+        .replace('\n', "");
     let cals_parsed: Vec<&str> = calories.split(',').collect();
     let mut cal_vec: Vec<i32> = Vec::new();
-    
+
     for cals in cals_parsed {
         let further_parsed_cals: String = cals.replace('\r', ",");
-        let reduced_cals: i32 = further_parsed_cals.split(',').map(|cal| cal.parse::<i32>().unwrap()).reduce(|a,b| a + b).unwrap();
+        let reduced_cals: i32 = further_parsed_cals
+            .split(',')
+            .map(|cal| cal.parse::<i32>().unwrap())
+            .reduce(|a, b| a + b)
+            .unwrap();
         cal_vec.push(reduced_cals);
     }
 
@@ -21,8 +28,14 @@ fn second_star(sorted_cals: Vec<i32>) -> i32 {
 
 fn main() {
     let sorted_cals: Vec<i32> = first_star();
-    println!("The largest amount of calories carried by one elf is {:#?}.", sorted_cals[sorted_cals.len() - 1]);
-    
+    println!(
+        "The largest amount of calories carried by one elf is {:#?}.",
+        sorted_cals[sorted_cals.len() - 1]
+    );
+
     let summed_top_three: i32 = second_star(sorted_cals);
-    println!("The sum of the top three elves calories is {:#?}.", summed_top_three);
+    println!(
+        "The sum of the top three elves calories is {:#?}.",
+        summed_top_three
+    );
 }
