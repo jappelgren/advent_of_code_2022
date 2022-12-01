4edf4164e17e65f3ca73bc38d917a61f0ee4d0aa
jappelgren <justinappelgren@gmail.com>
2022-12-01 12:47:11 -0600

    linted with clippy

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,18 +1,18 @@
 use std::fs;
 
 fn first_star() -> Vec<i32> {
-    let calories: String = fs::read_to_string(".\\src\\calories.txt").unwrap().replace("\r\n\r", ",").replace("\n", "");
-    let cals_parsed: Vec<&str> = calories.split(",").collect();
+    let calories: String = fs::read_to_string(".\\src\\calories.txt").unwrap().replace("\r\n\r", ",").replace('\n', "");
+    let cals_parsed: Vec<&str> = calories.split(',').collect();
     let mut cal_vec: Vec<i32> = Vec::new();
     
     for cals in cals_parsed {
-        let further_parsed_cals: String = cals.replace("\r", ",");
-        let reduced_cals: i32 = further_parsed_cals.split(",").map(|cal| cal.parse::<i32>().unwrap()).reduce(|a,b| a + b).unwrap();
+        let further_parsed_cals: String = cals.replace('\r', ",");
+        let reduced_cals: i32 = further_parsed_cals.split(',').map(|cal| cal.parse::<i32>().unwrap()).reduce(|a,b| a + b).unwrap();
         cal_vec.push(reduced_cals);
     }
 
     cal_vec.sort();
-    return cal_vec;
+    cal_vec
 }
 
 fn second_star(sorted_cals: Vec<i32>) -> i32 {
