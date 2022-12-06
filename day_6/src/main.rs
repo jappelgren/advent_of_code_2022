559c1184903567aadecdd28ffe16fcaf2e0de2ad
jappelgren <justinappelgren@gmail.com>
2022-12-06 09:05:24 -0600

    formatted and linted

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -9,10 +9,15 @@ fn first_star() -> usize {
     let input: String = input_to_string();
     let input_vec: Vec<char> = input.chars().collect();
 
-    let mut i:usize = 3;
+    let mut i: usize = 3;
 
     while i < input_vec.len() {
-        let mut cur_slice = vec!(input_vec[i - 3],input_vec[i - 2],input_vec[i - 1],input_vec[i]);
+        let mut cur_slice = vec![
+            input_vec[i - 3],
+            input_vec[i - 2],
+            input_vec[i - 1],
+            input_vec[i],
+        ];
         cur_slice.sort_unstable();
         cur_slice.dedup();
 
@@ -23,20 +28,20 @@ fn first_star() -> usize {
         i += 1;
     }
 
-    return 0;
+    0
 }
 
 fn second_star() -> usize {
     let input: String = input_to_string();
     let input_vec: Vec<char> = input.chars().collect();
 
-    let mut i:usize = 13;
+    let mut i: usize = 13;
 
     while i < input_vec.len() {
         let mut cur_slice: Vec<char> = Vec::new();
 
-        for j in i - 13..=i {
-            cur_slice.push(input_vec[j]);
+        for item in input_vec.iter().take(i + 1).skip(i - 13) {
+            cur_slice.push(*item);
         }
         cur_slice.reverse();
         cur_slice.sort_unstable();
@@ -49,7 +54,7 @@ fn second_star() -> usize {
         i += 1;
     }
 
-    return 0;
+    0
 }
 
 fn input_to_string() -> String {
