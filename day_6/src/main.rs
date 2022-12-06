f197f2a5e53db07f0baa6b9f84d632a96d74489f
jappelgren <justinappelgren@gmail.com>
2022-12-06 09:01:38 -0600

    easy one today, both stars

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,3 +1,57 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!("The result for the first star is: {:?}", first_star());
+    println!("The result for the second star is: {:?}", second_star());
+}
+
+fn first_star() -> usize {
+    let input: String = input_to_string();
+    let input_vec: Vec<char> = input.chars().collect();
+
+    let mut i:usize = 3;
+
+    while i < input_vec.len() {
+        let mut cur_slice = vec!(input_vec[i - 3],input_vec[i - 2],input_vec[i - 1],input_vec[i]);
+        cur_slice.sort_unstable();
+        cur_slice.dedup();
+
+        if cur_slice.len() == 4 {
+            return i + 1;
+        }
+
+        i += 1;
+    }
+
+    return 0;
+}
+
+fn second_star() -> usize {
+    let input: String = input_to_string();
+    let input_vec: Vec<char> = input.chars().collect();
+
+    let mut i:usize = 13;
+
+    while i < input_vec.len() {
+        let mut cur_slice: Vec<char> = Vec::new();
+
+        for j in i - 13..=i {
+            cur_slice.push(input_vec[j]);
+        }
+        cur_slice.reverse();
+        cur_slice.sort_unstable();
+        cur_slice.dedup();
+
+        if cur_slice.len() == 14 {
+            return i + 1;
+        }
+
+        i += 1;
+    }
+
+    return 0;
+}
+
+fn input_to_string() -> String {
+    fs::read_to_string(".\\src\\signal.txt").unwrap()
 }
